3e105b2b8292bcf99d55146e9b13454173177185
adam <jezek.adamek@seznam.cz>
2015-06-03 15:49:37 +0200

    commented example sketch

--- servodriver/servodriver.ino
+++ servodriver/servodriver.ino
@@ -1,11 +1,12 @@
 #include <Wire.h>
-#include <Adafruit_PWMServoDriver.h>
+#include <Adafruit_PWMServoDriver.h> //servo library included
 
-Adafruit_PWMServoDriver servoDriver = Adafruit_PWMServoDriver();
+Adafruit_PWMServoDriver servoDriver = Adafruit_PWMServoDriver(); //create servo driver object
 
 void setup() {
   Serial.begin(9600);
-  servoDriver.begin();
+  
+  servoDriver.begin(); //begin servodrover
   servoDriver.setPWMFreq(60);
   }
   
@@ -27,10 +28,10 @@ void loop() {
     
 }
     
-void setServo(int servonum, int servopos) {
-  int val = map(servopos, 0, 180, 150, 545);
-  Serial.print(servopos);
+void setServo(int servonum, int servopos) { //set servo (0-15 | servonum) position (0-180 | servopos)
+  int val = map(servopos, 0, 180, 150, 545); //remap 0-180 to servo positions on I2C driver (0=150, 180=545)
+  Serial.print(servopos); //print to serial port
   Serial.print(" - ");
   Serial.println(val);
-  servoDriver.setPWM(servonum,0 , val);
+  servoDriver.setPWM(servonum,0 , val); //set servo position
 }
